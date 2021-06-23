ee97702af1b3805f7ed197b8443a154e5bf27e28
Justin Lovinger <git@justinlovinger.com>
2021-06-23 15:40:37 -0400

    Change package name

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
 { pkgs ? import ./pkgs.nix { }, compiler ? "ghc8104" }:
-pkgs.haskell.packages.${compiler}.callCabal2nix "optimal-binary-accelerate" ./. { }
+pkgs.haskell.packages.${compiler}.callCabal2nix "optimal-hs-accelerate-binary" ./. { }
