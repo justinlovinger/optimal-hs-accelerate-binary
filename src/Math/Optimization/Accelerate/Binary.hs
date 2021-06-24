1e0592a8141f5974f7cf5401c71418744ba370fe
Justin Lovinger <git@justinlovinger.com>
2021-06-24 16:36:25 -0400

    Change function names
    for better clarity of purpose.

--- src/Math/Optimization/Accelerate/Binary.hs
+++ src/Math/Optimization/Accelerate/Binary.hs
@@ -2,15 +2,16 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Math.Optimization.Accelerate.Binary
-  ( fromBools
-  , fromBools'
+  ( reverseBitsToFrac
+  , reverseBitsToInt
   ) where
 
 import qualified Data.Array.Accelerate         as A
 
 -- | Reduce innermost dimension
 -- to numbers within lower and upper bound.
-fromBools
+-- Leftmost is least significant.
+reverseBitsToFrac
   :: (A.Shape sh, A.Fractional a, A.FromIntegral Int a)
   => A.Exp a -- ^ Lower bound
   -> A.Exp a -- ^ Upper bound
@@ -18,10 +19,10 @@ fromBools
   -> A.Acc (A.Array sh a)
 -- Guard on empty
 -- to avoid division by 0.
-fromBools lb ub bs = A.acond
+reverseBitsToFrac lb ub bs = A.acond
   (nb A.== 0)
   (A.fill (A.indexTail sh) lb)
-  ((a *! A.map A.fromIntegral (fromBools' bs)) !+ lb)
+  ((a *! A.map A.fromIntegral (reverseBitsToInt bs)) !+ lb)
  where
   a  = (ub - lb) / (2 A.^ nb - 1) -- range / max int
   nb = A.indexHead sh
@@ -30,11 +31,11 @@ fromBools lb ub bs = A.acond
 -- | Reduce innermost dimension
 -- to base 10 integer representations of bits.
 -- Leftmost is least significant.
-fromBools'
+reverseBitsToInt
   :: (A.Shape sh)
   => A.Acc (A.Array (sh A.:. Int) Bool)
   -> A.Acc (A.Array sh Int)
-fromBools' = A.sum . A.imap (\i x -> A.boolToInt x * 2 A.^ A.indexHead i)
+reverseBitsToInt = A.sum . A.imap (\i x -> A.boolToInt x * 2 A.^ A.indexHead i)
 
 -- | Add scalar to each element of an array.
 (!+)
--- test/Math/Optimization/Accelerate/BinarySpec.hs
+++ test/Math/Optimization/Accelerate/BinarySpec.hs
@@ -4,8 +4,8 @@ import qualified Data.Array.Accelerate         as A
 import qualified Data.Array.Accelerate.Interpreter
                                                as AI
 import           Math.Optimization.Accelerate.Binary
-                                                ( fromBools
-                                                , fromBools'
+                                                ( reverseBitsToFrac
+                                                , reverseBitsToInt
                                                 )
 import           Test.Hspec                     ( Spec
                                                 , describe
@@ -21,97 +21,98 @@ spec =
     $ describe "Accelerate"
     $ describe "Binary"
     $ do
-        describe "fromBools" $ do
+        describe "reverseBitsToFrac" $ do
           let ub' lb x = lb + abs x
           it "returns lower bound for empty" $ property $ \lb x ->
-            fromBoolsL lb (ub' lb x) [] == lb
+            reverseBitsToFracL lb (ub' lb x) [] == lb
           it "returns lower bound when all False" $ property $ \lb x ->
             let ub = ub' lb x
-            in  fromBoolsL lb ub [False]
+            in  reverseBitsToFracL lb ub [False]
                   ~= lb
-                  && fromBoolsL lb ub [False, False]
+                  && reverseBitsToFracL lb ub [False, False]
                   ~= lb
-                  && fromBoolsL lb ub [False, False, False]
+                  && reverseBitsToFracL lb ub [False, False, False]
                   ~= lb
           it "returns upper bound when all True" $ property $ \lb x ->
             let ub = ub' lb x
-            in  fromBoolsL lb ub [True]
+            in  reverseBitsToFracL lb ub [True]
                   ~= ub
-                  && fromBoolsL lb ub [True, True]
+                  && reverseBitsToFracL lb ub [True, True]
                   ~= ub
-                  && fromBoolsL lb ub [True, True, True]
+                  && reverseBitsToFracL lb ub [True, True, True]
                   ~= ub
           it "returns a number between lower and upper bound when some True"
             $ property
             $ \lb x ->
                 let ub       = ub' lb x + 0.001
                     between' = between lb ub
-                in  between' (fromBoolsL lb ub [False, True])
-                      && between' (fromBoolsL lb ub [True, False])
-                      && between' (fromBoolsL lb ub [True, False, True])
+                in  between' (reverseBitsToFracL lb ub [False, True])
+                      && between' (reverseBitsToFracL lb ub [True, False])
+                      && between' (reverseBitsToFracL lb ub [True, False, True])
           it "monotonically increases as binary increases"
             $ property
             $ \lb x ->
                 let ub = ub' lb x + 0.001
-                in  fromBoolsL lb ub [True]
-                      >  fromBoolsL lb ub [False]
-                      && fromBoolsL lb ub [True, False]
-                      >  fromBoolsL lb ub [False, False]
-                      && fromBoolsL lb ub [False, True]
-                      >  fromBoolsL lb ub [True, False]
-                      && fromBoolsL lb ub [True, True]
-                      >  fromBoolsL lb ub [False, True]
-                      && fromBoolsL lb ub [True, False, False]
-                      >  fromBoolsL lb ub [False, False, False]
-                      && fromBoolsL lb ub [False, True, False]
-                      >  fromBoolsL lb ub [True, False, False]
-                      && fromBoolsL lb ub [True, True, False]
-                      >  fromBoolsL lb ub [False, True, False]
-                      && fromBoolsL lb ub [False, False, True]
-                      >  fromBoolsL lb ub [True, True, False]
-                      && fromBoolsL lb ub [True, False, True]
-                      >  fromBoolsL lb ub [False, False, True]
-                      && fromBoolsL lb ub [False, True, True]
-                      >  fromBoolsL lb ub [True, False, True]
-                      && fromBoolsL lb ub [True, True, True]
-                      >  fromBoolsL lb ub [False, True, True]
+                in  reverseBitsToFracL lb ub [True]
+                      >  reverseBitsToFracL lb ub [False]
+                      && reverseBitsToFracL lb ub [True, False]
+                      >  reverseBitsToFracL lb ub [False, False]
+                      && reverseBitsToFracL lb ub [False, True]
+                      >  reverseBitsToFracL lb ub [True, False]
+                      && reverseBitsToFracL lb ub [True, True]
+                      >  reverseBitsToFracL lb ub [False, True]
+                      && reverseBitsToFracL lb ub [True, False, False]
+                      >  reverseBitsToFracL lb ub [False, False, False]
+                      && reverseBitsToFracL lb ub [False, True, False]
+                      >  reverseBitsToFracL lb ub [True, False, False]
+                      && reverseBitsToFracL lb ub [True, True, False]
+                      >  reverseBitsToFracL lb ub [False, True, False]
+                      && reverseBitsToFracL lb ub [False, False, True]
+                      >  reverseBitsToFracL lb ub [True, True, False]
+                      && reverseBitsToFracL lb ub [True, False, True]
+                      >  reverseBitsToFracL lb ub [False, False, True]
+                      && reverseBitsToFracL lb ub [False, True, True]
+                      >  reverseBitsToFracL lb ub [True, False, True]
+                      && reverseBitsToFracL lb ub [True, True, True]
+                      >  reverseBitsToFracL lb ub [False, True, True]
           it "converts each row in a matrix to a number" $ do
             AI.run
-                (fromBools (A.constant 0) (A.constant 3) $ A.use $ A.fromList
-                  (A.Z A.:. 3 A.:. 2)
-                  [False, False, True, False, False, True]
+                ( reverseBitsToFrac (A.constant 0) (A.constant 3)
+                $ A.use
+                $ A.fromList (A.Z A.:. 3 A.:. 2)
+                             [False, False, True, False, False, True]
                 )
               `shouldBe` A.fromList (A.Z A.:. (3 :: Int)) [0 :: Double, 1, 2]
 
-        describe "fromBools'" $ do
+        describe "reverseBitsToInt" $ do
           it "returns 0 when empty" $ do
-            fromBoolsL' [] `shouldBe` 0
+            reverseBitsToIntL [] `shouldBe` 0
           it "returns the base 10 integer represented by binary bits" $ do
-            fromBoolsL' [False] `shouldBe` 0
-            fromBoolsL' [False, False] `shouldBe` 0
-            fromBoolsL' [False, False, False] `shouldBe` 0
-            fromBoolsL' [True] `shouldBe` 1
-            fromBoolsL' [True, True] `shouldBe` 3
-            fromBoolsL' [True, True, True] `shouldBe` 7
+            reverseBitsToIntL [False] `shouldBe` 0
+            reverseBitsToIntL [False, False] `shouldBe` 0
+            reverseBitsToIntL [False, False, False] `shouldBe` 0
+            reverseBitsToIntL [True] `shouldBe` 1
+            reverseBitsToIntL [True, True] `shouldBe` 3
+            reverseBitsToIntL [True, True, True] `shouldBe` 7
           it "treats leftmost as least significant" $ do
-            fromBoolsL' [False, True] `shouldBe` 2
-            fromBoolsL' [False, False, True] `shouldBe` 4
+            reverseBitsToIntL [False, True] `shouldBe` 2
+            reverseBitsToIntL [False, False, True] `shouldBe` 4
           it "converts each row in a matrix to a number" $ do
             AI.run
-                (fromBools' $ A.use $ A.fromList
+                (reverseBitsToInt $ A.use $ A.fromList
                   (A.Z A.:. 3 A.:. 2)
                   [False, False, True, False, False, True]
                 )
               `shouldBe` A.fromList (A.Z A.:. (3 :: Int)) [0 :: Int, 1, 2]
 
-fromBoolsL :: Double -> Double -> [Bool] -> Double
-fromBoolsL lb ub xs = head $ A.toList $ AI.run $ fromBools
+reverseBitsToFracL :: Double -> Double -> [Bool] -> Double
+reverseBitsToFracL lb ub xs = head $ A.toList $ AI.run $ reverseBitsToFrac
   (A.constant lb)
   (A.constant ub)
   (A.use $ A.fromList (A.Z A.:. length xs) xs)
 
-fromBoolsL' :: [Bool] -> Int
-fromBoolsL' xs = head $ A.toList $ AI.run $ fromBools'
+reverseBitsToIntL :: [Bool] -> Int
+reverseBitsToIntL xs = head $ A.toList $ AI.run $ reverseBitsToInt
   (A.use $ A.fromList (A.Z A.:. length xs) xs)
 
 (~=) :: (Floating a, Ord a) => a -> a -> Bool
